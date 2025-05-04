19717dbb2dad5ce540798fb6d6be38a82ffe6b17
Lucas <113099777+lucaskooijmans@users.noreply.github.com>
2025-05-04 16:14:31 +0200

    Update index.blade.php

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -15,7 +15,24 @@
 {{--    --}}
 {{--@endforeach--}}
 <h3>
-    Ik heb goed nieuws: U bent dood.
+    We hebben eindelijk nieuwe stoelen!!!
 </h3>
+<p>
+    4 mei 2025
+</p>
+
+<h3>
+    Ja het restaurant is vol tijdens kerst man niet meer komen man
+</h3>
+<p>
+    20 december 2024
+</p>
+
+<h3>
+    Vanuit China helemaal naar Den Bosch gekomen, what's up
+</h3>
+<p>
+    5 juni 2007
+</p>
 
 <x-layout2/>
